e34a45ed5bc08acd9a2da371011b636e2bd3190f
Josh Davis <jdavisja@gmail.com>
2026-02-14 18:05:31 -0500

    Create HandsEngineConfig class in config.py

---
+++ services/hands-engine/hands_engine/core/config.py
@@ -0,0 +1,8 @@
+class HandsEngineConfig:
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def some_method(self):
+        # Implementation here
+        pass
